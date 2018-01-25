ab7fb98f6345cb933a77a01c14d9e9cec8382dbc
lburgazzoli <lburgazzoli@gmail.com>
2018-01-25 16:57:49 +0100

    Refactor extension-log to leverage annotations

--- syndesis-extension-log/src/main/java/io/syndesis/extension/LogExtension.java
+++ syndesis-extension-log/src/main/java/io/syndesis/extension/LogExtension.java
@@ -1,17 +1,18 @@
 package io.syndesis.extension;
 
+import io.syndesis.extension.api.SyndesisActionProperty;
+import io.syndesis.extension.api.SyndesisExtensionAction;
 import io.syndesis.extension.api.SyndesisStepExtension;
 import org.apache.camel.CamelContext;
 import org.apache.camel.model.ProcessorDefinition;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Optional;
 
+@SyndesisExtensionAction(id = "log", name = "log", description = "Log data.")
+@SyndesisActionProperty(name = "message", description = "The logging message", displayName = "message", type = "textarea")
 public class LogExtension implements SyndesisStepExtension {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LogExtension.class);
-
+    
     @Override
     public Optional<ProcessorDefinition> configure(CamelContext context, ProcessorDefinition route, Map<String, Object> parameters) {
         String message = (String) parameters.get("message");
