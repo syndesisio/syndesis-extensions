703656fc48653b8d458c23a6d5194753fdb59609
Andrea Cosentino <ancosen@gmail.com>
2018-02-01 13:16:21 +0100

    Add Header extension

---
+++ syndesis-extension-add-header/src/main/java/io/syndesis/extension/AddHeaderExtension.java
@@ -0,0 +1,73 @@
+package io.syndesis.extension;
+
+import java.util.Map;
+import java.util.Optional;
+
+import io.syndesis.extension.api.SyndesisActionProperty;
+import io.syndesis.extension.api.SyndesisExtensionAction;
+import io.syndesis.extension.api.SyndesisStepExtension;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.model.ProcessorDefinition;
+import org.apache.camel.util.ObjectHelper;
+
+@SyndesisExtensionAction(id = "addHeader", name = "Add Header", description = "Add an header")
+public class AddHeaderExtension implements SyndesisStepExtension {
+
+    // ************************
+    // Extension Properties
+    // ************************
+    
+    @SyndesisActionProperty(
+        name = "name",
+        displayName = "Header name",
+        description = "The header name to work on",
+        type = "string" ,
+        required = true)
+    private String name;
+
+    @SyndesisActionProperty(
+        name = "value",
+        displayName = "Header Value",
+        description = "The header value",
+        type = "string" ,
+        required = true)
+    private String value;
+    
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+    // ************************
+    // Extension
+    // ************************
+
+    @Override
+    public Optional<ProcessorDefinition> configure(CamelContext context, ProcessorDefinition route, Map<String, Object> parameters) {
+        ObjectHelper.notNull(route, "route");
+        ObjectHelper.notNull(name, "name");
+        ObjectHelper.notNull(value, "value");
+
+        return Optional.of(route.process(this::process));
+    }
+
+	void process(Exchange exchange) throws Exception {
+
+    	Message message = exchange.hasOut() ? exchange.getOut() : exchange.getIn();
+    	message.setHeader(name, value);
+    }
+}
