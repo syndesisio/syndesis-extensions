ef1afe647b58962b197dc53e8eaff7c574a09ea4
Andrea Cosentino <ancosen@gmail.com>
2018-02-07 11:27:52 +0100

    Added Loop extension

---
+++ syndesis-extension-loop/src/main/java/io/syndesis/extension/LoopExtension.java
@@ -0,0 +1,47 @@
+package io.syndesis.extension;
+
+import java.util.Map;
+import java.util.Optional;
+
+import io.syndesis.extension.api.SyndesisActionProperty;
+import io.syndesis.extension.api.SyndesisExtensionAction;
+import io.syndesis.extension.api.SyndesisStepExtension;
+import org.apache.camel.CamelContext;
+import org.apache.camel.model.ProcessorDefinition;
+import org.apache.camel.util.ObjectHelper;
+
+@SyndesisExtensionAction(id = "loop", name = "Loop", description = "Add a loop to your exchange")
+public class LoopExtension implements SyndesisStepExtension {
+
+    // ************************
+    // Extension Properties
+    // ************************
+    
+    @SyndesisActionProperty(
+        name = "cycles",
+        displayName = "Cycles",
+        description = "The loop cycles to apply to the route",
+        type = "int" ,
+        required = true)
+    private int cycles;
+
+    // ************************
+    // Extension
+    // ************************
+    
+	public int getCycles() {
+		return cycles;
+	}
+
+	public void setCycles(int cycles) {
+		this.cycles = cycles;
+	}
+
+	@Override
+    public Optional<ProcessorDefinition> configure(CamelContext context, ProcessorDefinition route, Map<String, Object> parameters) {
+        ObjectHelper.notNull(route, "route");
+        ObjectHelper.notNull(cycles, "cycles");
+
+        return Optional.of(route.loop(cycles));
+    }
+}
