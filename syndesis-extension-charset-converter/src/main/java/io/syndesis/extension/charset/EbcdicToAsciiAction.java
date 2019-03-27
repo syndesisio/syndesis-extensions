179b901ab8ea1ca1634af339dbcc1cae8d5b1061
Josh Reagan <jreagan@redhat.com>
2019-03-27 09:22:16 -0600

    Added character set converter step extension.

---
+++ syndesis-extension-charset-converter/src/main/java/io/syndesis/extension/charset/EbcdicToAsciiAction.java
@@ -0,0 +1,46 @@
+package io.syndesis.extension.charset;
+
+import java.util.Map;
+import java.util.Optional;
+
+import io.syndesis.extension.api.Step;
+import io.syndesis.extension.api.annotations.Action;
+import io.syndesis.extension.api.annotations.ConfigurationProperty;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.model.ProcessorDefinition;
+import org.apache.camel.util.ObjectHelper;
+
+@Action(id = "ebcdicToAscii", name = "EBCDIC To ASCII Converter", description = "Convert text from the EBCDIC to the ASCII character set", tags = {"extension"})
+public class EbcdicToAsciiAction implements Step {
+
+  @ConfigurationProperty(
+          name = "charset",
+          displayName = "Charcter set",
+          description = "The type of EBCDIC charcter set to use",
+          type = "String",
+          required = true,
+          enums = {
+            @ConfigurationProperty.PropertyEnum(label = "Latin-1 (IBM1047)", value = "Cp1047"),
+            @ConfigurationProperty.PropertyEnum(label = "500V1 (IBM500)", value = "Cp500"),
+            @ConfigurationProperty.PropertyEnum(label = "Latin-1 (IBM037)", value = "Cp037")})
+  private String charset;
+
+  public String getCharset() {
+    return charset;
+  }
+
+  public void setCharset(String charset) {
+    this.charset = charset;
+  }
+
+  @Override
+  public Optional<ProcessorDefinition> configure(CamelContext context, ProcessorDefinition route, Map<String, Object> parameters) {
+    ObjectHelper.notNull(route, "route");
+    ObjectHelper.notNull(charset, "charset");
+
+    return Optional.of(route.process((Exchange exchange) -> {
+      exchange.getIn().setBody(new String(exchange.getIn().getBody(String.class).getBytes("US-ASCII"), charset));
+    }));
+  }
+}
