a41d94e31ac4c9572ec279c5d62ad3a88cb55ede
Andrea Cosentino <ancosen@gmail.com>
2019-10-30 12:02:14 +0100

    Fixed camel-extension-cache

--- syndesis-extension-cache/src/test/java/io/syndesis/extension/cache/HelloBean.java
+++
@@ -1,9 +0,0 @@
-package io.syndesis.extension.cache;
-
-public class HelloBean {
-    public static int counter = 0;
-
-    public String work(String body){
-        return String.format("Hello %d: %s", counter++, body);
-    }
-}
