903ca3f12c0c564bba9b4d323d7f0160e7b9e9de
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2020-04-20 14:55:42 +0200

    feat(steps): adding a validate EIP extensions
    I will use it to showcase the creation of a new extension

---
+++ syndesis-extension-validate/src/main/java/io/syndesis/extension/validate/ValidateAction.java
@@ -0,0 +1,42 @@
+package io.syndesis.extension.validate;
+
+import java.util.Map;
+import java.util.Optional;
+
+import io.syndesis.extension.api.Step;
+import io.syndesis.extension.api.annotations.Action;
+import io.syndesis.extension.api.annotations.ConfigurationProperty;
+import org.apache.camel.CamelContext;
+import org.apache.camel.model.ProcessorDefinition;
+import org.apache.camel.util.ObjectHelper;
+
+import static org.apache.camel.language.simple.SimpleLanguage.predicate;
+
+@Action(id = "validate", name = "validate", description = "Add a simple validation step to your exchange", tags = { "validate", "extension"})
+public class ValidateAction implements Step {
+    
+    @ConfigurationProperty(
+        name = "rule",
+        displayName = "Validation rule",
+        description = "The rule used to validate the incoming message",
+        type = "String" ,
+        required = true)
+    private String rule;
+
+    public String getRule(){
+        return rule;
+    }
+
+    public void setRule(String rule){
+        this.rule = rule;
+    }
+
+	@Override
+    public Optional<ProcessorDefinition> configure(CamelContext context, ProcessorDefinition route, Map<String, Object> parameters) {
+        ObjectHelper.notNull(route, "route");
+        ObjectHelper.notNull(rule, "rule");
+
+        return Optional.of(route.validate(predicate(rule)));
+    }
+
+}
