660a5e8b22abc175b2e43c127fe49de2690e74bb
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2020-04-24 10:53:53 +0200

    feat(extensions): align master to main syndesis
    * Extension master is now aligned to main syndesis master
    * Added some note to explain how to build an extension against a syndesis version
    * Added some note to explain the new release branch availability
    * Deprecated and removed old connectors from being built with extensions parent
    
    closes #85

--- syndesis-extension-cache/src/main/java/io/syndesis/extension/cache/CacheAction.java
+++ syndesis-extension-cache/src/main/java/io/syndesis/extension/cache/CacheAction.java
@@ -9,11 +9,12 @@ import org.apache.camel.AsyncCallback;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.apache.camel.NamedNode;
 import org.apache.camel.Processor;
 import org.apache.camel.model.PipelineDefinition;
 import org.apache.camel.model.ProcessorDefinition;
-import org.apache.camel.processor.DelegateAsyncProcessor;
 import org.apache.camel.spi.InterceptStrategy;
+import org.apache.camel.support.processor.DelegateAsyncProcessor;
 import org.apache.camel.util.ObjectHelper;
 
 import io.syndesis.extension.api.Step;
@@ -52,12 +53,14 @@ public class CacheAction implements Step {
         final AtomicReference<Message> cachedValue = new AtomicReference<>();
 
         final String cahceStepId = route.getId();
-        context.addInterceptStrategy(new InterceptStrategy() {
+
+        route.addInterceptStrategy(new InterceptStrategy() {
 
             boolean countingDown = false;
             int remaining = steps;
 
-            public Processor wrapProcessorInInterceptors(CamelContext context, ProcessorDefinition<?> definition, Processor target, Processor nextTarget) throws Exception {
+            @Override
+            public Processor wrapProcessorInInterceptors(CamelContext context, NamedNode definition, Processor target, Processor nextTarget) throws Exception {
                 System.out.println("intercept pipeline " + definition.getId() + ", " + cahceStepId);
 
                 if (definition instanceof PipelineDefinition) {
